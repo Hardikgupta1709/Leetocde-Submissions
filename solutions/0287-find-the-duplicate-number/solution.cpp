60c6bbb274c5ad5755e701a741af60b6d5f935c3
Hardik Gupta <hardikgupta506@gmail.com>
2026-02-04 15:38:22 +0000

    Sync LeetCode submission - Find the Duplicate Number - Runtime - 34 ms (28.94%), Memory - 65.1 MB (70.04%)

---
+++ solutions/0287-find-the-duplicate-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size()+1;
+
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+};
