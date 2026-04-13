79e368d197b642c9761e7830e5623f81db71210c
Hardik Gupta <hardikgupta506@gmail.com>
2026-04-13 11:37:44 +0000

    Sync LeetCode submission - Minimum Distance to the Target Element - Runtime - 0 ms (100.00%), Memory - 13.3 MB (69.72%)

---
+++ solutions/1848-minimum-distance-to-the-target-element/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        
+        int mini = INT_MAX;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] == target)
+            {
+                int val = abs(i-start);
+                mini = min(mini,val);
+            }   
+        }
+    return mini;
+    }
+};
