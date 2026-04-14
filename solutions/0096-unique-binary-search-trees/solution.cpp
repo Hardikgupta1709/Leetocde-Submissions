2dad98e9d3f6cb58498d88d902d9a6f9f64f70d2
Hardik Gupta <hardikgupta506@gmail.com>
2026-04-14 13:08:29 +0000

    Sync LeetCode submission - Unique Binary Search Trees - Runtime - 0 ms (100.00%), Memory - 7.7 MB (90.09%)

---
+++ solutions/0096-unique-binary-search-trees/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        
+        long long res =1;
+
+        for(int i=1; i<=n; i++)
+        {
+            res = res * (n+i);
+            res = res/i;
+        }
+
+        return res/(n+1);
+    }
+};
