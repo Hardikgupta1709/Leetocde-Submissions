f782b790960ceb29f936a0abfcaa5d8016c316a1
Hardik Gupta <hardikgupta506@gmail.com>
2026-05-17 02:53:34 +0000

    Sync LeetCode submission - Count K-th Roots in a Range - Runtime - 0 ms (100.00%), Memory - 8.9 MB (40.72%)

---
+++ solutions/3932-count-k-th-roots-in-a-range/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countKthRoots(int l, int r, int k) {
+
+        int left = ceil(pow(l,1.0/k) -1e-9);
+        int right = floor(pow(r,1.0/k) +1e-9);
+
+        return max(0, right-left+1);
+    }
+};
