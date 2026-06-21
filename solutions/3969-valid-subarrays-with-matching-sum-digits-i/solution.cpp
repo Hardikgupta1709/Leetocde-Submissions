ff6cb60943089708480f8171477e6c40c938deb3
Hardik Gupta <hardikgupta506@gmail.com>
2026-06-21 03:04:45 +0000

    Sync LeetCode submission - Valid Subarrays With Matching Sum Digits I - Runtime - 147 ms (83.60%), Memory - 32.3 MB (71.71%)

---
+++ solutions/3969-valid-subarrays-with-matching-sum-digits-i/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countValidSubarrays(vector<int>& nums, int x) {
+
+        int count =0;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            long long sum =0;
+            for(int j=i; j<nums.size(); j++)
+            {
+                sum += nums[j];
+
+                int lastDigit = sum%10;
+
+                if(lastDigit != x)
+                {
+                     continue;   
+                }
+
+                long long temp = sum;
+
+                while(temp>=10)
+                {
+                    temp /=10;
+                }
+
+                int firstDigit = temp;
+
+                if(firstDigit == x)
+                {
+                    count ++;
+                }
+            }
+        }
+    return count;
+    }
+};
