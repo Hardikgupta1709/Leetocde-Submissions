1cdee7d33f6d2dd9c1bcbfa066b305f4b28d0d7c
Hardik Gupta <hardikgupta506@gmail.com>
2026-07-28 05:03:54 +0000

    Sync LeetCode submission - Smallest Palindromic Rearrangement I - Runtime - 139 ms (16.76%), Memory - 53.9 MB (94.22%)

---
+++ solutions/3517-smallest-palindromic-rearrangement-i/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+       int n = s.size();
+
+       if(n == 3 || n ==2 || n == 1)
+       {
+            return s;
+       }
+
+       if(n%2 == 0)
+       {
+            sort(s.begin(), s.begin()+n/2);
+            sort(s.begin()+n/2, s.end(),greater<char>());
+       }
+       else
+       {
+            sort(s.begin(), s.begin()+(n/2));
+            sort(s.begin()+(n/2)+1, s.end(), greater<char>());
+       }
+    return s;
+    }
+};
