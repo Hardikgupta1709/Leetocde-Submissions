985a20b99ed955b1fe50a8cb7475490ca7b73eb5
Hardik Gupta <hardikgupta506@gmail.com>
2026-08-02 02:48:27 +0000

    Sync LeetCode submission - Count Subarrays With Even Odd Ratio I - Runtime - 35 ms (86.63%), Memory - 29.8 MB (54.49%)

---
+++ solutions/4011-count-subarrays-with-even-odd-ratio-i/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countRatioSubarrays(vector<int>& nums, int a, int b) {
+        int count =0;
+
+        for(int i=0; i<nums.size(); i++)
+            {
+                int even =0;
+                int odd =0;
+                for(int j=i; j<nums.size(); j++)
+                    {
+                        if(nums[j] %2 == 0)
+                        {
+                            even++;
+                        }
+                        else
+                        {
+                            odd ++;
+                        }
+                        
+                        if(odd>0 && (1LL * even * b <= 1LL * odd * a))
+                        {
+                            count++;
+                        }
+                    }
+            }
+        return count;
+    }
+};
